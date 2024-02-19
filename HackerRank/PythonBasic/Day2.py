9a76e165cfcb584b1d5132b389eecd9057f9a4e2
YoniKim0319 <129754535+YoniKim0319@users.noreply.github.com>
2024-02-19 20:07:17 +0900

    Create Day2.py

---
+++ HackerRank/PythonBasic/Day2.py
@@ -0,0 +1,7 @@
+# Task 4: Print Function
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        result = str(i)
+        print(result, end= '')
+'''!! 반복문의 결과를 가로로 출력하기 위해선, end= '' 가 필요''' 
